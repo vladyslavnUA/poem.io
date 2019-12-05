6ca75484f18fdd62d157a18b110d01158c8656fb
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2019-12-05 01:05:18 -0800

    updated poetry.py

--- poetry.py
+++ poetry.py
@@ -4,14 +4,35 @@ if __name__ == '__main__':
     f = open("poem.txt", 'r')
     poem = f.readlines()
 
-print()
-for i in range(len(poem), 0, -1):
-    index = str(i)
-    line = poem[i-1]
-    print(index + ":" + line)
+def index_poem():
+  print()
+  for i in range(len(poem), 0, -1):
+      index = str(i)
+      line = poem[i-1]
+      print(index + ":" + line)
+      print()
+      
+def shuffle_poem():
+  print("####################################")
+  print()
+  lines = [ line for line in poem ]
+  random.shuffle(lines)
+  for line in lines:
+      print(line)
+      print()
 
-print()
-lines = [ line for line in poem ]
-random.shuffle(lines)
-for line in lines:
-    print(line)
+def custom_shuffle():
+  print("####################################")
+  print()
+  line = [ line for line in poem ]
+  for line in line:
+    words = [ word for word in line ]
+    random.shuffle(words)
+    for word in words:
+      print (word, end=" ")
+    
+    print()
+
+index_poem()
+shuffle_poem()
+custom_shuffle()
